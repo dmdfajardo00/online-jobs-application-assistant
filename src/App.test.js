3700ecfd9684cff87218ba3d9aa0ac70006d1e1d
dmdfajardo00 <dfajardo@tranches.com>
2025-07-27 19:14:04 +0800

    Initial vercel deployment

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders upwork assistant header', () => {
+test('renders online jobs assistant header', () => {
   render(<App />);
-  const headerElement = screen.getByText(/upwork ai application assistant/i);
+  const headerElement = screen.getByText(/online jobs ai assistant/i);
   expect(headerElement).toBeInTheDocument();
 });
